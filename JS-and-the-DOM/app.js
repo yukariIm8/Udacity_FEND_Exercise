d1aa8b9c1dcb1153088596053189bb88b1961960
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-07-14 11:36:40 -0700

    feat: added DOM manipulation.

---
+++ JS-and-the-DOM/app.js
@@ -0,0 +1,71 @@
+document.addEventListener('keypress', function() {
+    console.log('removing first child');
+    document.querySelector('#contain-all').firstElementChild.remove();
+});
+
+document.addEventListener('click', function(e) {
+    console.log(e);
+});
+
+
+
+const myCustomDiv = document.createElement('div');
+
+function respondToTheClick(evt) {
+    console.log('A paragraph was clicked: ' + evt.target.textContent);
+}
+
+for (let i = 1; i <= 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.textContent = 'This is paragraph number ' + i;
+
+    myCustomDiv.appendChild(newElement);
+}
+
+document.body.appendChild(myCustomDiv);
+
+myCustomDiv.addEventListener('click', respondToTheClick);
+
+
+
+const links = document.querySelectorAll('a');
+const thirdLink = links[2];
+
+thirdLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log("Look, ma! We didn't navigate to a new page!");
+});
+
+
+
+document.querySelector('#content').addEventListener('click', function (evt) {
+    if (evt.target.nodeName === 'SPAN') {  // ← verifies target is desired element
+        console.log('A span was clicked with text ' + evt.target.textContent);
+    }
+});
+
+
+
+const startingTime = performance.now();
+
+for (let i = 1; i <= 100; i++) { 
+  for (let j = 1; j <= 100; j++) {
+    console.log('i and j are ', i, j);
+  }
+}
+
+const endingTime = performance.now();
+console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
+
+
+
+const fragment = document.createDocumentFragment();  // ← uses a DocumentFragment instead of a <div>
+
+for (let i = 0; i < 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.innerText = 'This is paragraph number ' + i;
+
+    fragment.appendChild(newElement);
+}
+
+document.body.appendChild(fragment); // reflow and repaint here -- once!
