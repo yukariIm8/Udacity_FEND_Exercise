933cc634605d264b1278bc22968239e1b86e28b8
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-07-14 13:48:19 -0700

    starter code

---
+++ Client-side-and-Server-side/app.js
@@ -0,0 +1,25 @@
+/* Function to POST data */
+const postData = async ( url = '', data = {})=>{
+    console.log(data)
+      const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+  
+      try {
+        const newData = await response.json();
+        // console.log(newData);
+        return newData
+      }catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+      }
+  }
+  
+  // TODO-Call Function
+  
+  
