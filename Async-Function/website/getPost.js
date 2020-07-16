3271376cf7a3a0e595b775aa3fa77e3d550166a4
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-07-15 19:13:21 -0700

    feat: created an async function to make a GET request.

--- Async-Function/website/getPost.js
+++ Async-Function/website/getPost.js
@@ -20,3 +20,21 @@ const postData = async ( url = '', data = {}) => {
         console.log("error", error);
     }
 }
+
+// TODO-Async GET
+/*
+1) In the file getPost.js, write an async function to make a GET request that has one argument: a url to make the GET request to.
+
+Hint - Fetch is your friend.
+*/
+
+const getData = async (url = '') => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+    } catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
