eee7102c6900c5b7e91c8d9c33cf68628734bc90
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-07-15 23:17:10 -0700

    feat: created a function to chain POST and GET async functions.

--- Async-Function/website/getPost.js
+++ Async-Function/website/getPost.js
@@ -38,3 +38,21 @@ const getData = async (url = '') => {
         // appropriately handle the error
     }
 }
+
+// TODO-Chain your async functions to post an animal then GET the resulting data
+/*
+1) In the file getPost.js, write a function that chains together the two async functions you have previously written, so that you make a POST request to the /animal route, and then retrieve the data with a GET request to the /all path.
+
+You should pass in a data object of your favorite animal as the second argument for the POST request.
+*/
+function postGet() {
+    postData('/animal', {fav:'lion'})
+    .then(function(data){
+        retrieveData('/all')
+    })
+}
+
+
+// TODO-Call the chained function
+// Call your function as the last line of code in the file named
+postGet()
